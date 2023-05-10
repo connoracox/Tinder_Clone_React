96065a60c390ef03dc50da0de75d8f032a51f439
Connor Cox <connor.a.cox@gmail.com>
2023-05-10 14:04:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { BrowserRouter, 
+  Routes, 
+  Route, 
+  Link 
+} from 'react-router-dom'
+import Home from './views/Home'
+import Matches from './views/Matches';
+import Account from './views/Account';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/matches">Matches</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/views/Matches.js
@@ -0,0 +1,8 @@
+
+export default function Matches() {
+    return (
+        <div>
+            <h1>Matches</h1>
+        </div>
+    )
+}
